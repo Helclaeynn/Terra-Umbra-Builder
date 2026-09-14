710b1798f3450f9d3320b235e934aea08581a45b
Helclaeynn <malekith51@hotmail.com>
2026-09-14 16:33:05 +0200

    feat(compendium): add owner auth configuration

---
+++ compendium/github-editor-config.js
@@ -0,0 +1,9 @@
+export const GITHUB_EDITOR_CONFIG={
+  owner:'Helclaeynn',
+  repo:'Terra-Umbra-Builder',
+  repositoryId:'1158203380',
+  baseBranch:'main',
+  allowedLogin:'Helclaeynn',
+  clientId:'',
+  oauthProxy:''
+};
