95a5952c0175c164564ea98d8fe6939fcc54cba8
Helclaeynn <malekith51@hotmail.com>
2026-09-14 16:46:57 +0200

    config(compendium): set GitHub App client ID

--- compendium/github-editor-config.js
+++ compendium/github-editor-config.js
@@ -4,6 +4,6 @@ export const GITHUB_EDITOR_CONFIG={
   repositoryId:'1158203380',
   baseBranch:'main',
   allowedLogin:'Helclaeynn',
-  clientId:'',
+  clientId:'Iv23li5jsFHb19k5TavH',
   oauthProxy:''
 };
