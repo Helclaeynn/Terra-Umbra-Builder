44ddb945316d0d0f40dcecd33a0c1d8f83604e53
Helclaeynn <malekith51@hotmail.com>
2026-09-19 17:08:48 +0200

    feat(v2): route native compendium

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -2,6 +2,7 @@ import { createApp, h } from "vue";
 import { createRouter, createWebHistory, RouterView } from "vue-router";
 import App from "./App.vue";
 import CharacterBuilderPage from "./pages/CharacterBuilderPage.vue";
+import CompendiumPage from "./pages/CompendiumPage.vue";
 import "./style.css";
 
 const router=createRouter({
@@ -9,6 +10,7 @@ const router=createRouter({
   routes:[
     { path:"/", component:App },
     { path:"/characters/:id/builder", component:CharacterBuilderPage },
+    { path:"/compendium", component:CompendiumPage },
     { path:"/:pathMatch(.*)*", redirect:"/" }
   ]
 });
